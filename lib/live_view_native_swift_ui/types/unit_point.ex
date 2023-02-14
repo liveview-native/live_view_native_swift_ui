814271ae3cab2bd5f2bca7bc2af00c1c89884960
Carson Katri <Carson.katri@gmail.com>
2023-02-14 16:59:29 -0500

    Add error cast

--- lib/live_view_native_swift_ui/types/unit_point.ex
+++ lib/live_view_native_swift_ui/types/unit_point.ex
@@ -5,4 +5,5 @@ defmodule LiveViewNativeSwiftUi.Types.UnitPoint do
   def type, do: :map
 
   def cast(value) when is_map(value) or is_list(value), do: {:ok, struct(__MODULE__, value)}
+  def cast(_), do: :error
 end
